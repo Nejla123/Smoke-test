7efe3903cb14a2efae5ea72273fd6f98f9e7438a
Nejla <Nejla@10.32.6.83>
2018-11-18 16:19:02 +0100

    deleted some unnecessary classes

--- SeleniumSmokeTest/src/webbasic/proba.java
+++ SeleniumSmokeTest/src/webbasic/proba.java
@@ -1,11 +1,8 @@
 package webbasic;
 import org.testng.Assert;
-import org.testng.annotations.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
